18524ecb258cf2cb18db215dc3c638962cf28b18
wali computers <tayyabashahbaz2020@gmail.com>
2024-03-23 20:30:25 +0000

    adding more text

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    {message:"Enter first number", type:"number", name: "firstNumber"},
-     {message: "Enter second number", type: "number", name: "secondNumber"},
+    {message:"Enter your first number", type:"number", name: "firstNumber"},
+     {message: "Enter your second number", type: "number", name: "secondNumber"},
      {message: "select one of the operator to perform operation" ,
       type: "list",
        name: "operator",
@@ -19,6 +19,7 @@ if ( answer.operator ==="Addition") {
     console.log(answer.firstNumber - answer.secondNumber);
 } else if (answer.operator === "Multiplication") {
     console.log(answer.firstNumber * answer.secondNumber);
+
  } else if (answer.operator ==="Division") {
     console.log(answer.firstNumber / answer.secondNumber);}
     else { console.log("please select valid operator")}
